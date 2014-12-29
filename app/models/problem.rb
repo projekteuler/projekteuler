cbf17cfce07221844c86e189108c03fea21e45cc
Philipp Fischbeck <philipp.fischbeck@student.hpi.uni-potsdam.de>
2014-12-29 16:18:20 +0100

    Add delegation and per_page to problem

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,4 +1,8 @@
 class Problem < ActiveRecord::Base
   has_one :translation
+  delegate :title, :content, to: :translation
+
   has_many :translations, inverse_of: :problem
+  
+  self.per_page = 50
 end

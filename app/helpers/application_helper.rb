594f9540a8d487554f8902eba70ca9b2013ce299
Philipp Fischbeck <philipp.fischbeck@student.hpi.uni-potsdam.de>
2015-02-01 12:00:49 +0100

    Add tests for flash message helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,15 +4,17 @@ module ApplicationHelper
   end
 
   def flash_messages(opts = {})
-    flash.each do |msg_type, message|
-      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} alert-dismissible", role: 'alert') do
-               concat(content_tag(:button, class: 'close', data: { dismiss: 'alert' }) do
-                        concat content_tag(:span, '&times;'.html_safe, 'aria-hidden' => true)
-                        concat content_tag(:span, 'Close', class: 'sr-only')
-                      end)
-               concat message
-             end)
+    capture do
+      flash.each do |msg_type, message|
+        concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} alert-dismissible", role: 'alert') do
+          concat(content_tag(:button, class: 'close', data: { dismiss: 'alert' }) do
+            concat content_tag(:span, '&times;'.html_safe, 'aria-hidden' => true)
+            concat content_tag(:span, 'Close', class: 'sr-only')
+          end)
+          concat message
+        end)
+      end
+      nil
     end
-    nil
   end
 end

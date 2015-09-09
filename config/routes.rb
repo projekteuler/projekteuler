95b84f003c6df39965ce16a5df1761fd6e08d86b
Philipp Fischbeck <philipp.fischbeck@student.hpi.uni-potsdam.de>
2015-09-09 17:40:15 +0200

    Add problem count changing to the admin dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     get '', to: 'dashboard#index', as: '/'
+    post '/update_problem_count', to: 'dashboard#update_problem_count'
     resources :translations, only: [:index, :show]
   end
 
--- test/controllers/admin/dashboard_controller_test.rb
+++ test/controllers/admin/dashboard_controller_test.rb
@@ -12,4 +12,10 @@ class Admin::DashboardControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should post new problem count" do
+    post :update_problem_count, problem_count: 15
+    assert_redirected_to controller: 'admin/dashboard', action: 'index'
+    assert_equal 15, Problem.count
+  end
+
 end

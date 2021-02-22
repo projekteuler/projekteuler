2e2e41993803215d8f13117b727c32e995ebb812
Philipp Fischbeck <philipp.fischbeck@hpi.de>
2021-02-22 15:24:28 +0100

    Support underline in translations

--- app/scrubbers/translation_content_scrubber.rb
+++ app/scrubbers/translation_content_scrubber.rb
@@ -3,7 +3,7 @@ class TranslationContentScrubber < Rails::Html::PermitScrubber
     super
     self.tags = %w( strong em b i p code pre tt samp kbd var sub
         sup dfn cite big small address hr br div span h1 h2 h3 h4 h5 h6 ul ol li dl dt dd abbr
-        acronym a img blockquote del ins table th tr td thead tbody tfoot )
+        acronym a img blockquote del ins table th tr td thead tbody tfoot u )
     self.attributes = %w( href src width height alt cite datetime title class name xml:lang abbr style
         align border cellpadding cellspacing colspan )
   end
